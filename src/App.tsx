93ac7d6ddd56b8922dff359ea31f399c6ac415a0
unknown <sajib.shahariar97@gmail.com>
2021-07-17 12:10:41 +0600

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
+import TodoTask from "./components/TodoTask";
+import { ITask } from "./Intrefaces";
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadLine, setDeadLine] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+  const [error, setError] = useState<String>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.name === "task") {
+      setError("")
+      if (e.target.value.length === 0) {
+        setError("Fields can not be empty");
+      } else {
+        setError("");
+      }
+      setTask(e.target.value);
+    } else {
+      setDeadLine(Number(e.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    setError("")
+    if (!task && !deadLine) return setError("Fields can not be empty");
+    if (!task) return setError("Task can not be empty");
+    if (!deadLine) return setError("Deadline can not be 0");
+    const newTask = { taskName: task, deadLine: deadLine };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadLine(0);
+  };
+
+  const completeTask = (taskNameDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNameDelete;
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="task..."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline (in Days)..."
+            name="deadline"
+            value={deadLine}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      {error && <p style={{color: "red"}}><b>*{error}</b></p>}
+      <div className="todoList">
+        {todoList.map((task: ITask, key) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
